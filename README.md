f6a703604328e523aa779a357c88a2c8bfca7693
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-07-27 21:52:00 -0600

    chap 8 - error handling

--- README.md
+++ README.md
@@ -29,3 +29,78 @@ https://www.tutorialspoint.com/cprogramming/c_preprocessors.htm
 
 - `strcmp` returns 0 if strings are equal, else non-zero
 - `strstr` returns a pointer to the first occurence of the substring needle in the haystack. or a null pointer if the sequence is not present. the null terminators are not compared.
+- `strtol` converts a string to a `long int` in the given base
+  - `long int strtol(const char* nptr, char **endptr, int base);`
+- `strtod` converts string to double
+
+### Structs
+
+```c
+struct [structure tag] {
+
+   member definition;
+   member definition;
+   ...
+   member definition;
+} [one or more structure variables];
+```
+
+You can define pointers to structures in the same way as you define pointer to any other variable −
+
+`struct Books *struct_pointer;`
+
+Now, you can store the address of a structure variable in the above defined pointer variable. To find the address of a structure variable, place the '&'; operator before the structure's name as follows −
+
+`struct_pointer = &Book1;`
+
+To access the members of a structure using a pointer to that structure, you must use the → operator as follows −
+
+`struct_pointer->title;`
+
+### Enum
+
+An Enum can be declared in C as
+
+```c
+enum <enum_name> { const1, const2, ... }
+```
+
+### Error Handling in C
+
+- errno is a global variable that is set to non-zero by C if any error occurs
+- since it is global we must reset it before calling the error-prone code
+
+As such, C programming does not provide direct support for error handling but being a system programming language, it provides you access at lower level in the form of return values. Most of the C or even Unix function calls return -1 or NULL in case of any error and set an error code errno. It is set as a global variable and indicates an error occurred during any function call. You can find various error codes defined in <error.h> header file.
+
+So a C programmer can check the returned values and can take appropriate action depending on the return value. It is a good practice, to set errno to 0 at the time of initializing a program. A value of 0 indicates that there is no error in the program.
+
+More [here](https://www.tutorialspoint.com/cprogramming/c_error_handling.htm)
+
+### Union Data types
+
+A union is a special data type available in C that allows to store different data types in the same memory location.
+**You can define a union with many members, but only one member can contain a value at any given time.**
+Unions provide an efficient way of using the same memory location for multiple-purpose.
+
+To define a union, you must use the union statement in the same way as you did while defining a structure.
+
+```c
+union [union tag] {
+   member definition;
+   member definition;
+   ...
+   member definition;
+} [one or more union variables];
+
+union Data {
+   int i;
+   float f;
+   char str[20];
+} data;
+```
+
+#### When to use Union vs Structs
+
+https://www.quora.com/When-is-it-a-good-idea-to-use-union-instead-of-struct-in-C-programming?share=1
+
+Structs are like `Product` tyes, while Unions are like `Sum` types.
--- prompt.c
+++ prompt.c
@@ -28,56 +28,144 @@ void add_history(char *unused) {}
 
 #endif
 
-long eval_op(long x, char *op, long y)
+// Lisp Value
+typedef struct
 {
+	int type;
+	long num;
+	int err;
+} lval;
+
+// Create Enum of possible lval values
+enum
+{
+	LVAL_NUM,
+	LVAL_ERR
+};
+
+// Create Enum of possible error types
+enum lval_err_types
+{
+	LERR_DIV_ZERO,
+	LERR_BAD_OP,
+	LERR_BAD_NUM
+};
+
+// create a new number type lval
+lval lval_num(long x)
+{
+	lval v;
+	v.type = LVAL_NUM;
+	v.num = x;
+	return v;
+}
+
+// Create a new error type lval
+lval lval_err(int x)
+{
+	lval v;
+	v.type = LVAL_ERR;
+	v.err = x;
+	return v;
+}
+
+// Print an lval
+void lval_print(lval v)
+{
+	switch (v.type)
+	{
+	case LVAL_NUM:
+		printf("%li", v.num);
+		break;
+
+	case LVAL_ERR:
+		if (v.err == LERR_DIV_ZERO)
+		{
+			printf("Error: Division by zero");
+		}
+		if (v.err == LERR_BAD_OP)
+		{
+			printf("Error: Invalid Operation");
+		}
+		if (v.err == LERR_BAD_NUM)
+		{
+			printf("Error: Invalid Number");
+		}
+		break;
+	}
+}
+
+// Print an lval followed by a newline
+void lval_println(lval v)
+{
+	lval_print(v);
+	putchar('\n');
+}
+
+lval eval_op(lval x, char *op, lval y)
+{
+	// If either value is an error, return it
+	if (x.type == LVAL_ERR)
+	{
+		return x;
+	}
+	if (x.type == LVAL_ERR)
+	{
+		return y;
+	}
+
 	// strcmp returns 0 if strings are equal, else non-zero
 	if (strcmp(op, "+") == 0)
 	{
-		return x + y;
+		return lval_num(x.num + y.num);
 	}
 	if (strcmp(op, "*") == 0)
 	{
-		return x * y;
+		return lval_num(x.num * y.num);
 	}
 	if (strcmp(op, "-") == 0)
 	{
-		return x - y;
+		return lval_num(x.num - y.num);
 	}
 	if (strcmp(op, "/") == 0)
 	{
-		return x / y;
+		return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num);
 	}
 	if (strcmp(op, "%") == 0)
 	{
-		return x % y;
+		return lval_num(x.num % y.num);
 	}
 	if (strcmp(op, "^") == 0)
 	{
-		return pow(x, y);
+		return lval_num(pow(x.num, y.num));
 	}
-	return 0;
+
+	return lval_err(LERR_BAD_OP);
 }
 
-long eval(mpc_ast_t *t)
+lval eval(mpc_ast_t *t)
 {
 	// if tagged as number return it immediately
 	if (strstr(t->tag, "number"))
 	{
-		return atoi(t->contents);
+		// Check if there is any error in conversion
+		errno = 0;
+		long x = strtol(t->contents, NULL, 10);
+		return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
 	}
 
 	// if given the input "- 3", returns 3
-	if (strcmp(t->children[1]->contents, "-"))
-	{
-		long rest = eval(t->children[2]);
-		return -1 * rest;
-	}
+	// if (strcmp(t->children[1]->contents, "-"))
+	// {
+	// 	lval rest = eval(t->children[2]);
+	// 	return lval_num(-1 * rest.num);
+	// }
 
 	// the operator is always the second child
 	char *op = t->children[1]->contents;
 
 	// we store the third child in x
-	long x = eval(t->children[2]);
+	lval x = eval(t->children[2]);
 
 	// iterate on the remaining children and combining
 	int i = 3;
@@ -164,7 +252,6 @@ int main(int argc, char **argv)
 {
 
 	// Create some parsers
-
 	mpc_parser_t *Number = mpc_new("number");
 	mpc_parser_t *Operator = mpc_new("operator");
 	mpc_parser_t *Expr = mpc_new("expr");
@@ -173,13 +260,12 @@ int main(int argc, char **argv)
 	// Define the parers with the following language
 
 	mpca_lang(MPCA_LANG_DEFAULT,
-						"                                                \
+		"                                                \
 			number : /-?[0-9]+[.]?[0-9]*/ ;                          \
 			operator : '+' | '*' | '-' | '/' | '\%' | '^' |\"add\" | \"sub\" | \"mul\" | \"div\" ;       \
 			expr : <number> | '(' <operator> <expr>+ ')' | '-' <expr> ; \
 			lispy : /^/ <operator> <expr>+ /$/ ;           \
-		",
-						Number, Operator, Expr, Lispy);
+		", Number, Operator, Expr, Lispy);
 
 	puts("Lispy Version 0.0.0.0.1");
 	puts("Press Ctrl+c to Exit\n");
@@ -203,8 +289,8 @@ int main(int argc, char **argv)
 			int branches = longest_branch(r.output);
 			printf("Longest branch: %i\n", branches);
 
-			long result = eval(r.output);
-			printf("%li\n", result);
+			lval result = eval(r.output);
+			lval_println(result);
 
 			mpc_ast_delete(r.output);
 		}
